38f4bf87e7b3214d29d2e08985d7d53cda63d959
Aayush Bhatnagar <aayushbhatnagar1999@gmail.com>
2020-08-05 02:06:17 +0530

    all page except the last one is done

--- js/routes.js
+++ js/routes.js
@@ -11,54 +11,54 @@
                 url: '/',
                 templateUrl: 'components/home.html'
             })
-            // .state('dataStructure', {
-            //     url: '/dataStructure',
-            //     templateUrl: 'components/data.html',
-            //     controller: 'DataController as data',
-            //     resolve: {
-            //         items: ['CodeService', function (CodeService) {
-            //             return CodeService.getdata().then(function (items) {
-            //                 return items;
-            //             })
-            //         }]
-            //     }
-            // })
-            // .state('algorithm', {
-            //     url: '/algorithm',
-            //     templateUrl: 'components/algo.html',
-            //     controller: 'AlgoController as algo',
-            //     resolve: {
-            //         items: ['CodeService', function (CodeService) {
-            //             return CodeService.getalgo().then(function (items) {
-            //                 return items;
-            //             })
-            //         }]
-            //     }
-            // })
-            // .state('code', {
-            //     url: '/code/{type}',
-            //     templateUrl: 'components/code.html',
-            //     controller: 'CodeController as code',
-            //     resolve: {
-            //         items: ['$stateParams', 'CodeService', function ($stateParams, CodeService) {
-            //             return CodeService.getcodes($stateParams.type).then(function (items) {
-            //                 return items;
-            //             })
-            //         }]
-            //     }
-            // })
-            // .state('final', {
-            //     url: '/final/{num}',
-            //     templateUrl: 'components/final.html',
-            //     controller: 'FinalController as fin',
-            //     resolve: {
-            //         items: ['$stateParams', 'CodeService', function ($stateParams, CodeService) {
-            //             return CodeService.getfinal($stateParams.num).then(function (items) {
-            //                 return items;
-            //             })
-            //         }]
-            //     }
-            // })
+            .state('dataStructure', {
+                url: '/dataStructure',
+                templateUrl: 'components/data.html',
+                controller: 'DataController as data',
+                resolve: {
+                    items: ['CodeService', function (CodeService) {
+                        return CodeService.getdata().then(function (items) {
+                            return items;
+                        })
+                    }]
+                }
+            })
+            .state('algorithm', {
+                url: '/algorithm',
+                templateUrl: 'components/algo.html',
+                controller: 'AlgoController as algo',
+                resolve: {
+                    items: ['CodeService', function (CodeService) {
+                        return CodeService.getalgo().then(function (items) {
+                            return items;
+                        })
+                    }]
+                }
+            })
+            .state('code', {
+                url: '/code/{type}',
+                templateUrl: 'components/code.html',
+                controller: 'CodeController as code',
+                resolve: {
+                    items: ['$stateParams', 'CodeService', function ($stateParams, CodeService) {
+                        return CodeService.getcodes($stateParams.type).then(function (items) {
+                            return items;
+                        })
+                    }]
+                }
+            })
+            .state('final', {
+                url: '/final/{num}',
+                templateUrl: 'components/final.html',
+                controller: 'FinalController as fin',
+                resolve: {
+                    items: ['$stateParams', 'CodeService', function ($stateParams, CodeService) {
+                        return CodeService.getfinal($stateParams.num).then(function (items) {
+                            return items;
+                        })
+                    }]
+                }
+            })
 
     }
 }
